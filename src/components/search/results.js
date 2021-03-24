5030b135be636de8dc2b43ad57090ed1c7190915
Matt Watson <mattwat@gmail.com>
2021-03-24 09:26:29 -0400

    rethink search results component
    memoize search result sections
    
    refactor
    
    revert

--- src/components/search/results.js
+++ src/components/search/results.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect, useMemo, useState } from 'react'
 import { Section } from '../layout'
 import styled from 'styled-components'
 import { connectStateResults, Hits, Index } from 'react-instantsearch-dom'
@@ -23,38 +23,19 @@ const Results = ({ title, indexName, hitComponent }) => {
 }
 
 export const SearchResults = connectStateResults(({ searchState, searching, allSearchResults }) => {
-  console.log(allSearchResults, searching)
-
+  // useEffect(() => {
+  //   if (allSearchResults) {
+  //     console.log(allSearchResults)
+  //   }
+  // }, [allSearchResults])
 
   return (
-    searchState && searchState.query ? (
-      <Fragment key={ allSearchResults.nbhits || 'x' }>
-        {
-          allSearchResults.PEOPLE.nbHits > 0
-          && <Results title="People" indexName="PEOPLE" hitComponent={ PersonResult } />
-        }
-
-        {
-          allSearchResults.GROUPS.nbHits > 0
-          && <Results title="Groups" indexName="GROUPS" hitComponent={ GroupResult } />
-        }
-
-        {
-          allSearchResults.COLLABORATIONS.nbHits > 0
-          && <Results title="Collaborations" indexName="COLLABORATIONS" hitComponent={ CollaborationResult } />
-        }
-
-        {
-          allSearchResults.PROJECTS.nbHits > 0
-          && <Results title="Projects" indexName="PROJECTS" hitComponent={ ProjectResult } />
-        }
-
-        {
-          allSearchResults.NEWS.nbHits > 0
-          && <Results title="News" indexName="NEWS" hitComponent={ NewsResult }  />
-        }
-
+      <Fragment>
+        <Results title="People" indexName="PEOPLE" hitComponent={ PersonResult } />
+        <Results title="Groups" indexName="GROUPS" hitComponent={ GroupResult } />
+        <Results title="Collaborations" indexName="COLLABORATIONS" hitComponent={ CollaborationResult } />
+        <Results title="Projects" indexName="PROJECTS" hitComponent={ ProjectResult } />
+        <Results title="News" indexName="NEWS" hitComponent={ NewsResult } />
       </Fragment>
-    ) : null
   )}
 )
