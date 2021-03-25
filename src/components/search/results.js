f428c094804deab125934b56ff21a767aed82b4f
Matt Watson <mattwat@gmail.com>
2021-03-25 11:34:01 -0400

    conditionally display search indices' results

--- src/components/search/results.js
+++ src/components/search/results.js
@@ -12,30 +12,78 @@ const StyledHits = styled(Hits)`
   }
 `
 
-const Results = ({ title, indexName, hitComponent }) => {
+const IndexResults = connectStateResults(({ searchState, searchResults, children }) => {
+  return searchResults && searchResults.nbHits !== 0
+    ? children
+    : <span />
+})
+
+const AllResults = connectStateResults(({ allSearchResults, children }) => {
+  const hasResults = allSearchResults && Object.values(allSearchResults).some(results => results.nbHits > 0)
+  if (hasResults) {
+    return children
+  }
   return (
-    <Section title={ title }>
-      <Index indexName={ indexName }>
-        <StyledHits hitComponent={ hitComponent }/>
-      </Index>
-    </Section>
+    <div>
+      <div>No results.</div>
+      <Index indexName="PEOPLE" />
+      <Index indexName="GROUPS" />
+      <Index indexName="COLLABORATIONS" />
+      <Index indexName="PROJECTS" />
+      <Index indexName="NEWS" />
+    </div>
   )
-}
+})
 
-export const SearchResults = connectStateResults(({ searchState, searching, allSearchResults }) => {
-  // useEffect(() => {
-  //   if (allSearchResults) {
-  //     console.log(allSearchResults)
-  //   }
-  // }, [allSearchResults])
-
+export const SearchResults = connectStateResults(({ searchState, searching, allSearchResults, searchResults }) => {
+  if (!searchState.query) {
+    return <div />
+  }
   return (
-      <Fragment>
-        <Results title="People" indexName="PEOPLE" hitComponent={ PersonResult } />
-        <Results title="Groups" indexName="GROUPS" hitComponent={ GroupResult } />
-        <Results title="Collaborations" indexName="COLLABORATIONS" hitComponent={ CollaborationResult } />
-        <Results title="Projects" indexName="PROJECTS" hitComponent={ ProjectResult } />
-        <Results title="News" indexName="NEWS" hitComponent={ NewsResult } />
-      </Fragment>
-  )}
-)
+    <AllResults>
+      <Index indexName="PEOPLE">
+        <IndexResults>
+          <Section title="People">
+            <StyledHits hitComponent={ PersonResult } />
+          </Section>
+        </IndexResults>
+      </Index>
+
+
+      <Index indexName="GROUPS">
+        <IndexResults>
+          <Section title="Groups">
+            <StyledHits hitComponent={ GroupResult } />
+          </Section>
+        </IndexResults>
+      </Index>
+
+
+      <Index indexName="COLLABORATIONS">
+        <IndexResults>
+          <Section title="Collaborations">
+            <StyledHits hitComponent={ CollaborationResult } />
+          </Section>
+        </IndexResults>
+      </Index>
+
+
+      <Index indexName="PROJECTS">
+        <IndexResults>
+          <Section title="Projects">
+            <StyledHits hitComponent={ ProjectResult } />
+          </Section>
+        </IndexResults>
+      </Index>
+
+
+      <Index indexName="NEWS">
+        <IndexResults>
+          <Section title="News">
+            <StyledHits hitComponent={ NewsResult } />
+          </Section>
+        </IndexResults>
+      </Index>
+    </AllResults>
+  )
+})
