c88ca37c03d3e138dfc80970d1386ad186bb7347
Matt Watson <mattwat@gmail.com>
2021-06-15 17:35:47 -0400

    render loading message

--- src/components/data-table.js
+++ src/components/data-table.js
@@ -6,7 +6,9 @@ import ReactDataTable from 'react-data-table-component'
 export const DataTable = ({ columns, data, ...props }) => {
   const [query, ] = useState('')
 
-  if (!data) return null
+  if (!data) {
+    return 'Loading projects...'
+  }
 
   const tableProps = {
     data: data,
